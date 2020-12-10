20348153abe24719aa7b3c3b9f8392d11dcdef20
apurba_debnath <apurba871@gmail.com>
2020-12-10 09:04:57 +0530

    Implemented cross-off feature

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -1,1 +1,32 @@
-alert('Connected!');
+//alert('Connected!');
+
+//Check off specific todo by clicking
+
+// $("li").click(function() {
+// 	// if li is gray
+// 		// turn it black
+// 	// else
+// 		// turn it gray
+// 	//console.log($(this).css("color"));
+// 	if ($(this).css("color") === "rgb(128, 128, 128)") {
+// 		// $(this).css("color", "black");
+// 		// $(this).css("text-decoration", "none");
+// 		$(this).css({
+// 			color: "black",
+// 			textDecoration: "none"
+// 		});
+// 	}
+	
+// 	else {
+// 		// $(this).css("color", "gray");
+// 		// $(this).css("text-decoration", "line-through");
+// 		$(this).css({
+// 			color: "gray",
+// 			textDecoration: "line-through"
+// 		});
+// 	}
+// })
+
+$("li").click(function() {
+	$(this).toggleClass("completed");
+})
